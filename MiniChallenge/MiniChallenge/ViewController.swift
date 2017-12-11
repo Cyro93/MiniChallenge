b36b6ccccbfbd70976f5feecfd48a28265b7345c
Michele Golino <golinomichele@10-20-49-94.0200.fed.it.iosda.org>
2017-12-11 16:47:34 +0100

    i've add sum function

--- MiniChallenge/MiniChallenge/ViewController.swift
+++ MiniChallenge/MiniChallenge/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOne: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let v1 = kk()
+        let v2 = tt()
+        labelOne.text  = String(sum(tt: v2, kk: v1))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func kk () {
+    func kk () -> Int {
         let a  = 3
-        print (a)
+        return a
+    }
+    
+    func tt () -> Int {
+        let lol = 5
+        return lol
+    }
+    
+    
+    func sum (tt:Int, kk:Int  ) -> Int {
+        return tt + kk
     }
     
     override func didReceiveMemoryWarning() {
