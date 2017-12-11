10b023fc0ab628ad0f13f5a9579c7cf647fb8ca5
Michele Golino <golinomichele@10-20-49-94.0200.fed.it.iosda.org>
2017-12-11 17:39:13 +0100

    I've delet all functions e we show a clean project

--- MiniChallenge/MiniChallenge/ViewController.swift
+++ MiniChallenge/MiniChallenge/ViewController.swift
@@ -10,30 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var labelOne: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let v1 = kk()
-        let v2 = tt()
-        labelOne.text  = String(sum(tt: v2, kk: v1))
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func kk () -> Int {
-        let a  = 3
-        return a
-    }
-    
-    func tt () -> Int {
-        let lol = 5
-        return lol
-    }
-    
-    
-    func sum (tt:Int, kk:Int  ) -> Int {
-        return tt + kk
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
